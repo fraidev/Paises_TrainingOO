4ee9730bd7143df50ba4cc605fe661f108cdfec5
Frai <fraifelipe@gmail.com>
2018-07-24 21:25:09 -0300

    Init

---
+++ Paises.Tests/PaisTests.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using Paises.Domain;
+
+namespace Paises.Tests
+{
+    public class PaisTests
+    {
+        [Test]
+        public void Deve_Criar_Pais()
+        {
+            var brasil = new Pais("Brasil", "Brasilia", 500);
+            
+            Assert.AreEqual(brasil.Nome,"Brasil" );
+        }
+    }
+}
---
+++ Paises/Domain/Pais.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+
+namespace Paises.Domain
+{
+    public class Pais
+    {
+        public string Nome { get; set; }
+        public string Capital { get; set; }
+        public double DimensaoEmKm2 { get; set; }
+        public IList<Pais> PaisesEmFronteira { get; set; }
+
+        public Pais(string nome, string capital, double dimensaoEmKm2)
+        {
+            Nome = nome;
+            Capital = capital;
+            DimensaoEmKm2 = dimensaoEmKm2;
+        }
+
+        public string GetNome()
+        {
+            return Nome;
+        }
+        
+        public string GetCapital()
+        {
+            return Capital;
+        }
+        
+        public double GetDimensaoEmKm2()
+        {
+            return DimensaoEmKm2;
+        }
+
+        public bool SaoIguais(Pais outroPais)
+        {
+            return (Nome == outroPais.Nome && Capital == outroPais.Capital);
+        }
+    }
+}
